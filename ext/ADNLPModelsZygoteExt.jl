b97ff8dca39414c78bd09fe24ea39641f745cebf
Alexis Montoison <alexis.montoison@polymtl.ca>
2024-11-27 11:53:49 -0600

    Update the extensions

--- ext/ADNLPModelsZygoteExt.jl
+++ ext/ADNLPModelsZygoteExt.jl
@@ -2,21 +2,21 @@ module ADNLPModelsZygoteExt
 
 using Zygote, ADNLPModels
 
-function gradient(::ZygoteADGradient, f, x)
+function gradient(::ADNLPModels.ZygoteADGradient, f, x)
   g = Zygote.gradient(f, x)[1]
   return g === nothing ? zero(x) : g
 end
-function gradient!(::ZygoteADGradient, g, f, x)
+function gradient!(::ADNLPModels.ZygoteADGradient, g, f, x)
   _g = Zygote.gradient(f, x)[1]
   g .= _g === nothing ? 0 : _g
 end
 
-function Jprod!(::ZygoteADJprod, Jv, f, x, v, ::Val)
+function Jprod!(::ADNLPModels.ZygoteADJprod, Jv, f, x, v, ::Val)
   Jv .= vec(Zygote.jacobian(t -> f(x + t * v), 0)[1])
   return Jv
 end
 
-function Jtprod!(::ZygoteADJtprod, Jtv, f, x, v, ::Val)
+function Jtprod!(::ADNLPModels.ZygoteADJtprod, Jtv, f, x, v, ::Val)
   g = Zygote.gradient(x -> dot(f(x), v), x)[1]
   if g === nothing
     Jtv .= zero(x)
@@ -26,14 +26,14 @@ function Jtprod!(::ZygoteADJtprod, Jtv, f, x, v, ::Val)
   return Jtv
 end
 
-function jacobian(::ZygoteADJacobian, f, x)
+function jacobian(::ADNLPModels.ZygoteADJacobian, f, x)
   return Zygote.jacobian(f, x)[1]
 end
 
-function hessian(b::ZygoteADHessian, f, x)
+function hessian(b::ADNLPModels.ZygoteADHessian, f, x)
   return jacobian(
-    ForwardDiffADJacobian(length(x), f, x0 = x),
-    x -> gradient(ZygoteADGradient(), f, x),
+    ADNLPModels.ForwardDiffADJacobian(length(x), f, x0 = x),
+    x -> gradient(ADNLPModels.ZygoteADGradient(), f, x),
     x,
   )
 end
